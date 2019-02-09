881ea9d1c5f55bf22f7425b0e09a752c419908af
Carlos Nah <cnah27@gmail.com>
2019-02-09 14:02:04 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+const recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return  Object.assign({},object,{[key]:value})
+}
+
+function  destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  
+  return object
+}
